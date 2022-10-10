05fefd27bf74efc789b6ed5c8adc20dddcfb519d
Alhajideen <salifunurudeen834@gmail.com>
2022-10-10 17:45:22 -0600

    add Scores.js file

--- src/index.js
+++ src/index.js
@@ -1,27 +1,5 @@
 import './style.scss';
-import Icon from './img/icon-pwa.png';
+import Scores from './modules/Scores';
 
-function component() {
-  const el = document.getElementById('entry');
-  const element = document.createElement('div');
-  const media = document.createElement('img');
-  const code = document.createElement('code');
-  const link = document.createElement('a');
-  link.href = 'https://webpack.org';
-  link.target = '_blank';
-  link.innerText = 'official Website documentation';
-
-  element.innerHTML = 'Hello There, This is webpack!!!';
-  element.classList.add('hello');
-  media.classList.add('media');
-  el.appendChild(element);
-  media.src = Icon;
-  code.innerText = 'to learn more, visit the ';
-  code.appendChild(link);
-  el.appendChild(media);
-  el.appendChild(code);
-
-  return el;
-}
-
-document.body.appendChild(component());
+const tbody = document.querySelector('.tbody');
+tbody.innerHTML = Scores.render();
---
+++ src/modules/Scores.js
@@ -0,0 +1,27 @@
+export default class Dom {
+  static render() {
+    const tdata = `
+   <tr>
+                  <td>1</td>
+                  <td>Name:</td>
+                  <td>100</td>
+                </tr>
+                 <tr>
+                  <td>2</td>
+                  <td>Name:</td>
+                  <td>90</td>
+                </tr>
+                 <tr>
+                  <td>3</td>
+                  <td>Name:</td>
+                  <td>80</td>
+                </tr>
+                 <tr>
+                  <td>4</td>
+                  <td>Name:</td>
+                  <td>70</td>
+                </tr>
+  `;
+    return tdata;
+  }
+}
