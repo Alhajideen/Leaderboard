a7a5e336622fd87b4a9be0337bd2216894e2cea7
Alhajideen <salifunurudeen834@gmail.com>
2022-10-11 16:45:18 -0600

    add a success or failure message on submit

--- src/modules/Requests.js
+++ src/modules/Requests.js
@@ -3,18 +3,33 @@ import Data from './Data.js';
 
 class Requests {
   static async sendData() {
+    const sucess = document.querySelector('.sucess');
     const name = document.querySelector('.name');
     const mark = document.querySelector('.score');
     const user = name.value;
     const scores = mark.value;
     const newData = new Data(user, scores);
-    const data = await axios.post(
-      'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/kvZLvGVUqYif8ehySShj/scores/',
-      newData
-    );
-    console.log(data);
+    try {
+      const data = await axios.post(
+        'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/kvZLvGVUqYif8ehySShj/scores/',
+        newData
+      );
+      Requests.getData();
+      sucess.style.color = 'green';
+      sucess.innerHTML = 'Score added succesfully :)';
+      setTimeout(() => {
+        sucess.innerHTML = '';
+      }, 2000);
+    } catch (err) {
+      console.error(err.message);
+      sucess.style.color = 'red';
+      sucess.innerHTML = 'Oops something is not right';
+      setTimeout(() => {
+        sucess.innerHTML = '';
+      }, 2000);
+    }
   }
-  
+
   static async getData() {
     try {
       const res = await axios.get(
