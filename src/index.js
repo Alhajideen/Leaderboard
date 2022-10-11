d93824358e7c87119370671f058dbc008eae792c
Alhajideen <salifunurudeen834@gmail.com>
2022-10-11 16:18:24 -0600

    implement the refresh button to refresh list when clicked

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import Requests from './modules/Requests';
 
 //Variables
 const submitBtn = document.querySelector('.submit-btn');
+const refresh = document.querySelector('.refresh-btn');
 
 // Invoke functions
 Requests.getData();
@@ -10,4 +11,8 @@ Requests.getData();
 //Events
 submitBtn.addEventListener('click', () => {
   Requests.sendData();
+});
+
+refresh.addEventListener('click', () => {
+  Requests.getData();
 });
