48a871b87a47cac5f83853d652c338a6e7c2d775
Alhajideen <salifunurudeen834@gmail.com>
2022-10-11 16:14:47 -0600

    implement the sendData function to receive data from input and sent to api

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 import './style.scss';
 import Requests from './modules/Requests';
 
+//Variables
+const submitBtn = document.querySelector('.submit-btn');
+
 // Invoke functions
 Requests.getData();
+
+//Events
+submitBtn.addEventListener('click', () => {
+  Requests.sendData();
+});
--- src/modules/Requests.js
+++ src/modules/Requests.js
@@ -1,14 +1,31 @@
 import Scores from './Scores.js';
+import Data from './Data.js';
 
 class Requests {
-  static sendData() {}
-  static async getData() {
-    const res = await axios.get(
-      'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/kvZLvGVUqYif8ehySShj/scores/'
+  static async sendData() {
+    const name = document.querySelector('.name');
+    const mark = document.querySelector('.score');
+    const user = name.value;
+    const scores = mark.value;
+    const newData = new Data(user, scores);
+    const data = await axios.post(
+      'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/kvZLvGVUqYif8ehySShj/scores/',
+      newData
     );
-    const data = await res.data.result;
-    const tbody = document.querySelector('.tbody');
-    tbody.innerHTML = Scores.render(data);
+    console.log(data);
+  }
+  
+  static async getData() {
+    try {
+      const res = await axios.get(
+        'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/kvZLvGVUqYif8ehySShj/scores/'
+      );
+      const data = await res.data.result;
+      const tbody = document.querySelector('.tbody');
+      tbody.innerHTML = Scores.render(data);
+    } catch (err) {
+      console.error(err);
+    }
   }
 }
 export default Requests;
